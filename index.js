a624fb339269b7a0b62d72b816d15aead06687b4
jamesonrb2013 <jamesonrb2013@gmail.com>
2026-06-06 12:38:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,26 @@ app.use(passport.session());
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
+app.get(
+    "/auth/discord",
+    passport.authenticate("discord")
+);
+
+app.get(
+    "/auth/discord/callback",
+    passport.authenticate("discord", {
+        failureRedirect: "/"
+    }),
+    (req, res) => {
+        res.redirect("/dashboard");
+    }
+);
+
+app.get("/logout", (req, res) => {
+    req.logout(() => {
+        res.redirect("/");
+    });
+});
 
 // Home page
 app.get("/", (req, res) => {
